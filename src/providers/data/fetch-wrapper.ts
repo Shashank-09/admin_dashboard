724d8468d173a9ea6bdce08b04de76ba9bc8c637
Shashank-09 <shashanktulsikar979@gmail.com>
2024-02-24 23:54:38 +0530

    custome done

--- src/providers/data/fetch-wrapper.ts
+++ src/providers/data/fetch-wrapper.ts
@@ -46,8 +46,18 @@ const getGraphQLErrors = (body: Record<"errors" , GraphQLFormattedError[] | unde
     return null
 }
 
-const fetchWrapper = async(url:string , options: RequestInit) => {
+export const fetchWrapper = async(url:string , options: RequestInit) => {
   const response = await customFetch(url , options);
 
   const responseClone = response.clone();
+  const body = await responseClone.json();
+
+  const error = getGraphQLErrors(body);
+
+  if(error)
+  {
+    throw error;
+  }
+
+  return response;
 }
